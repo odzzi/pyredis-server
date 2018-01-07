0ca60ebbfa8d0f5649eeaf4f187b61ffa7e93b14
billy0920 <billy.wuweibin@gmail.com>
2018-01-07 13:16:20 +0800

    desc: add threading lock to storage.
    Signed-off-by: billy0920 <billy.wuweibin@gmail.com>

--- src/redis_server/store.py
+++ src/redis_server/store.py
@@ -1,16 +1,25 @@
+import threading
 
 data = {}
+LOCK = threading.Lock()
+
 
 def set(key, value):
-    data[key] = value
+    if LOCK.acquire():
+        data[key] = value
+        LOCK.release()
+
 
 def get(key):
     return data.get(key, None)
 
+
 def DEL(keys):
     ret = 0
-    for key in keys:
-        if data.get(key):
-            del data[key]
-            ret += 1
+    if LOCK.acquire():
+        for key in keys:
+            if data.get(key):
+                del data[key]
+                ret += 1
+        LOCK.release()
     return ret
