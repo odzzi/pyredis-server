95eea7227d61d026773186b009dc47edc90739ee
billy0920 <billy.wuweibin@gmail.com>
2018-01-11 22:53:17 +0800

    desc: import logging to log trace. import sys to resolve the command line args for host and port.
    Signed-off-by: billy0920 <billy.wuweibin@gmail.com>

--- test/test_set.py
+++ test/test_set.py
@@ -1,10 +1,9 @@
 
 
-def test_redis_server(n=1, HOST="127.0.0.1", PORT=6379):
+def test_redis_server(n=1, host="127.0.0.1", port=6379):
     import redis
     import time
-    r = redis.StrictRedis(host=HOST, port=6379, db=0)
-    # r = redis.Redis(host=HOST, port=PORT, db=0)
+    r = redis.StrictRedis(host=host, port=port, db=1)
     stime = time.time()
     try:
         for x in range(n):
@@ -12,6 +11,7 @@ def test_redis_server(n=1, HOST="127.0.0.1", PORT=6379):
             r.get('foo_%s' % x)
             r.delete('foo_%s' % x)
             r.dump('foo_%s' % x)
+            r.keys('foo_%s' % x)
     except Exception,e:
         #print e
         pass
@@ -19,12 +19,18 @@ def test_redis_server(n=1, HOST="127.0.0.1", PORT=6379):
 
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-
+    import sys
     import threading
+    HOST = "127.0.0.1"
+    PORT = 6379
+    if len(sys.argv) == 2:
+        PORT = int(sys.argv[1])
+    elif len(sys.argv) == 3:
+        HOST = sys.argv[1]
+        PORT = sys.argv[2]
     threads = []
-    for x in range(100):
-        t = threading.Thread(target=test_redis_server, args=(100,))
+    for x in range(1):
+        t = threading.Thread(target=test_redis_server, args=(1, HOST, PORT))
         threads.append(t)
 
     for thread in threads:
